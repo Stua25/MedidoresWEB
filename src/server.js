8a87a92ff448f4943ca83608d5482e118caa3581
Stuart <stuartcor10@gmail.com>
2018-10-19 22:22:06 -0600

    -agregar mysql -agregar otro registro

--- src/server.js
+++ src/server.js
@@ -2,19 +2,14 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
   bodyParser = require('body-parser'),
-  cors = require('cors'),
-  mongoose = require('mongoose');
+  cors = require('cors')
   var history = require('connect-history-api-fallback');
 
-//   { DB } = require('./config/DB'),
+
   itemRoutes = require('./routes/item');
   institucionesRoutes = require('./routes/institucion');
   
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(DB, { useMongoClient: true})
-//   .then(() => console.log('Db is conencted'))
-//   .catch(err => console.error(err));
 
 const app = express();
 var port = process.env.PORT || 4000;
